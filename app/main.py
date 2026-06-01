7d550cec7c168773abc527bf8fedad26f74afd88
황병현 <hwangbh@nextrunners.co.kr>
2026-06-01 11:47:27 +0900

    Fix: add mdeia & static

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from fastapi import FastAPI
@@ -11,6 +12,13 @@ app = FastAPI()
 app.include_router(user_router)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# 만약 static, media 폴더가 존재하지 않으면 생성
+if not (BASE_DIR / "static").exists():
+    os.mkdir(BASE_DIR / "static")
+if not (BASE_DIR / "media").exists():
+    os.mkdir(BASE_DIR / "media")
+
 # 'static' 폴더를 '/static' 경로로 마운트 (CSS, JS 파일 서빙용)
 app.mount("/static", StaticFiles(directory=BASE_DIR / "static"), name="static")
 # 'media' 폴더를 '/media' 경로로 마운트 (사용자 업로드 파일 서빙용)
